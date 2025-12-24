426f89bb71a9c98bdb6238152bf61f3fa7a384c8
bacuratec <btec6768@gmail.com>
2025-12-24 14:46:49 +0300

    45656567

--- next.config.js
+++ next.config.js
@@ -43,6 +43,13 @@ const nextConfig = {
         tls: false,
       };
     }
+    
+    // Ensure path aliases work correctly
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, './src'),
+    };
+    
     return config;
   },
 
